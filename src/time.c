7db1022e252a39738cad04a3db8646c0b0cee9e8
RealSys <admin@skysy.cn>
2018-10-20 18:20:28 +0800

    Updated.

--- src/time.c
+++ src/time.c
@@ -13,4 +13,5 @@ uint64_t what_time_pass(struct timeval tv)
 
     uint64_t t_now = now.tv_sec * 1000 + now.tv_usec / 1000;  
     uint64_t t_tv  = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+    return t_now - t_tv;
 }
